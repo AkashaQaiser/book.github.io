062068ae93953a6113ad5e8c0ec09b10f4b24781
AkashaQaiser <135351586+AkashaQaiser@users.noreply.github.com>
2023-06-02 21:27:46 +0500

    update done

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function toggleMenue(displayState){
+    const nav = document.getElementById('navigation');
+    const closeico = document.getElementById('close-ico');
+
+    nav.style.display = displayState ;
+    closeico.style.display = displayState ;
+}
